eeb803cc45a42fcc478f6ccd3777f3136d1920b8
bugrov.and <bugrovandriy@yandex.ru>
2023-03-11 15:15:17 +0300

    small changes

--- src/greedy_alg.py
+++ src/greedy_alg.py
@@ -4,7 +4,7 @@ import numpy as np
 def greedy_alg(matr: np.array) -> np.float64:
     n = matr.len()
     greedy_sum = np.float64(0)
-    # на случай если по столбцам
+    # iterating by columns: P[i] - array for every parti of i-th step
     for i in range(n):
         max_in_column = matr[i][0]
         for j in range(1, n):
@@ -12,11 +12,4 @@ def greedy_alg(matr: np.array) -> np.float64:
                 max_in_column = matr[i][0]
         greedy_sum += max_in_column
     return greedy_sum
-    # на случай если по строкам
-    # for j in range(0,n):
-    #     max_in_column=matr[0][j]
-    #     for i in range(1,n):
-    #         if matr[i][j] > max_in_column:
-    #             max_in_column = matr[i][0]
-    #     greedy_sum += max_in_column
-    # return greedy_sum
+# Пожелания к интерфейсу: гарантия поступления на вход заполненной матрицы с типом данных float64
